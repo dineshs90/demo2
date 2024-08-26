dc51ef77215e880532edea5d49c117abbdfd8432
Dinesh <dinesh.shanmugaraj@walmart.com>
2024-08-26 20:42:26 +0530

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dineshs90/demo2
 
 go 1.23.0
+
+require github.com/dineshs90/demo3 v0.0.0-20240826145327-024ab8515ec5 // indirect
--- test.go
+++ test.go
@@ -1,9 +1,22 @@
 package test
 
+import (
+	dog "github.com/dineshs90/demo3"
+)
+
 func Bark() string {
 	return "Woof !!!"
 }
 
 func Barks() string {
 	return "Dog Barks!!!"
+
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
